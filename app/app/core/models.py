38e38de30373e39d9f3d254367e9b32d174f06b9
Jenya <jekab596@gmail.com>
2024-04-22 14:04:51 +0300

    Creation of article app, model and test for this modle

--- app/app/core/models.py
+++ app/app/core/models.py
@@ -1,10 +1,8 @@
 """ 
 Database models.
 """
-
+from django.conf import settings
 from django.db import models
-
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
@@ -41,3 +39,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Article(models.Model):
+    """Article model object."""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- app/app/core/tests/test_models.py
+++ app/app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models.
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
 class ModelTests(TestCase):
@@ -42,3 +43,10 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_article(self):
+        """Test creating an article is successful."""
+        user = get_user_model().objects.create_user('test@example.com', 'testpass123')
+        article = models.Article.objects.create(
+            user=user, title="Test title", content="Test article content.")
+        self.assertEqual(str(article), article.title)
